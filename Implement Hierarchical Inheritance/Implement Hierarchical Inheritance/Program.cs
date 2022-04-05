2499adc04efb1acd00a84fb429867efd6f6f2db6
sadik789 <73237396+sadik789@users.noreply.github.com>
2022-04-05 13:53:49 +0530

    C# Console Program.

---
+++ Implement Hierarchical Inheritance/Implement Hierarchical Inheritance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Implement_Hierarchical_Inheritance
+{
+    class A
+    {
+        public int a;
+        public A()
+        {
+            a=10;
+        }
+    }
+    class B:A
+    {
+        public int b;
+        public B()
+        {
+           b=15;
+        }
+    }
+        class C:A
+        {
+            public int c;
+            public C()
+            {
+                c=20;
+            }
+        }
+    class program
+    
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hierchical Inheritance :");
+            B obj1 = new B();
+            C obj2 = new C();
+            Console.WriteLine("Using Class B object(obj1)");
+            Console.WriteLine("a={0}", obj1.a);
+            Console.WriteLine("b={0}", obj1.b);
+            Console.WriteLine("Using Class C ject(obj2");
+            Console.WriteLine("a={0}", obj2.a);
+            Console.WriteLine("b={0}", obj2.c);
+
+            Console.ReadKey();
+        }
+    }
+}
