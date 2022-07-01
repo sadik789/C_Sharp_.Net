51d16fc1b703f0a97ae9ceef10139e7b8df64ee6
sadik789 <73237396+sadik789@users.noreply.github.com>
2022-07-01 20:15:49 +0530

    Add files via upload

---
+++ 01 Addition Of Two Number/01 Addition Of Two Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_Addition_Of_Two_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2,Total;
+
+            Console.WriteLine("Enter First Number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Total = num1 + num2;
+
+            Console.WriteLine("Total is : "+ Total);
+            Console.ReadKey();
+
+        }
+    }
+}
